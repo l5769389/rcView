d1f142cfc8501703821dde15dc95b76166f14b43
5769389@qq.com <5769389@qq.com>
2023-11-22 20:36:07 +0800

    save

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -3,25 +3,14 @@ import { Peer } from 'peerjs'
 import { ref } from 'vue'
 
 const peer = new Peer('1', {
-  host: '192.168.3.243',
+  host: '192.168.31.52',
   port: 9000
 })
 const connect2ServerFlag = ref(false)
-peer.on('open',() => {
+peer.on('open', () => {
+  console.log('open')
   connect2ServerFlag.value = true
 })
-
-peer.on('connection', (conn) => {
-  console.log('connect')
-  conn.on('data', (data) => {
-    // Will print 'hi!'
-    console.log(data)
-  })
-  conn.on('open', () => {
-    conn.send('hello!')
-  })
-})
-
 
 const connect = () => {
   peer.connect('1')
@@ -30,7 +19,7 @@ const connect = () => {
 
 <template>
   <div>
-    连接到服务器：{{connect2ServerFlag}}
+    连接到服务器：{{ connect2ServerFlag }}
     <button @click="connect">连接到对方</button>
   </div>
 </template>
