86f0d715bd3c29430a4c9194311bbf9caf0bbb1a
ljun <5769389@qq.com>
2023-11-26 20:40:22 +0800

    save

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,4 @@
-interface ImportMetaEnv {
-  readonly MAIN_VITE_ROLE: string
-  readonly RENDERER_VITE_ROLE: string
-  readonly MAIN_VITE_ROLE_TEST: string
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
