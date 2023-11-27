5a42bdf92670c1ae696b39996ae865d4ea59e8bd
ljun <5769389@qq.com>
2023-11-27 21:23:03 +0800

    解决输入小键盘的问题

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ export default class Config {
   static readonly ROLE: 'SERVER' | 'CLIENT' = this.CLIENT
   static readonly SERVER_ID = '1'
 
-  static readonly SIGNAL_SERVER_IP = '192.168.3.243'
+  static readonly SIGNAL_SERVER_IP = '192.168.31.52'
+  // static readonly SIGNAL_SERVER_IP = '192.168.3.243'
   static readonly SIGNAL_SERVER_PORT = 9000
 
   static readonly IS_SERVER_WINDOW_SHOW = true
